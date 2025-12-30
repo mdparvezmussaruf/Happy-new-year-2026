947ca1175234972ffd29d7dbe8e29c3502203d36
MD Parvez Mussaruf <49514187+mdparvezmussaruf@users.noreply.github.com>
2025-12-30 13:06:52 +0600

    Fix formatting issues in service worker file

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener("install",e=>{
+ e.waitUntil(
+  caches.open("ny2026").then(c=>c.addAll([
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./music.mp3"
+]))
+ );
+});
+self.addEventListener("fetch",e=>{
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
